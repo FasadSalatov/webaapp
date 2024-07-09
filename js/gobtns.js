a085dedb5c3710d3787e63bd03a6924ec2853e5c
Fasad <salatiksama@gmail.com>
2024-07-09 21:31:23 +0300

    Add files via upload

--- js/gobtns.js
+++ js/gobtns.js
@@ -1,5 +1,21 @@
-document.querySelectorAll('#go').forEach(button => {
-    button.addEventListener('click', () => {
-        button.classList.toggle('clicked');
+document.addEventListener('DOMContentLoaded', function() {
+    const buttons = document.querySelectorAll('button[id^="go"]');
+
+    buttons.forEach((button, index) => {
+        const buttonId = `goButtonClicked_${index}`;
+
+        // Проверка состояния кнопки в localStorage
+        if (localStorage.getItem(buttonId) === 'true') {
+            button.classList.add('clicked');
+            button.disabled = true;
+        }
+
+        // Обработчик события для каждой кнопки
+        button.addEventListener('click', function(event) {
+            event.preventDefault(); // Предотвращение перехода по ссылке
+            button.classList.add('clicked');
+            button.disabled = true;
+            localStorage.setItem(buttonId, 'true');
+        });
     });
 });
